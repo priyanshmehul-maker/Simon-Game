9ecdf8aa9d71f8472fe483c8a300ef58328e4ced
priyanshmehul-maker <priyanshmehul@gmail.com>
2026-03-12 09:59:34 +0530

    Change event listener from keypress to touchstart

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let h2=document.querySelector("h2");
 let started=false;
 let level=0;
 
-document.addEventListener("keypress",function(){
+document.addEventListener("touchstart",function(){
     if(started==false){
         console.log("Game Started");
         started=true;
@@ -83,4 +83,5 @@ function reset(){
     userSeq=[];
 
 }
+
 
